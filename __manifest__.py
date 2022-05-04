7303718445becaeba8190dd72d8c86e9ac0a3fbe
valeriadudina <95287453+valeriadudina@users.noreply.github.com>
2022-05-05 01:52:16 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,6 @@
     "category": "Human Resources",
     "author": "Dudina Valeria",
     "category":"Productivity",
-    "depends": ["hr"],
     "data": ["views/x_studygroup_views.xml"],
     "installable": True,
 
